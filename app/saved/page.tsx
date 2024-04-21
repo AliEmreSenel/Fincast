bb6d0f5efdc15ad36ff71a96c2e422073f5c4b20
Ali Emre Senel <aliemre@senel.biz>
2024-04-21 09:50:20 +0200

    Implement saved page

---
+++ app/saved/page.tsx
@@ -0,0 +1,74 @@
+"use client"
+import { Bookmark } from "lucide-react";
+import { Card, CardContent, CardHeader, CardDescription, CardTitle } from "@/components/ui/card";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+let wishlist = [
+  "AAPL",
+  "TSLA",
+  "GOOGL"
+]
+
+export default function Component() {
+  let router = useRouter();
+  let [forecast, setForecast] = useState([]);
+  let [names, setNames] = useState([]);
+
+  useEffect(() => {
+    Promise.all(wishlist.map((stock) => {
+      return fetch("http://172.20.10.7/api/forecast/" + stock)
+        .then((res) => res.json())
+    })).then((data) => {
+      setForecast(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    Promise.all(wishlist.map((stock) => {
+      return fetch("http://172.20.10.7/api/forecast/" + stock + "/name")
+        .then((res) => res.json())
+    })).then((data) => {
+      setNames(data)
+    });
+  }, [])
+
+  if (forecast.length === 0 || names.length === 0)
+    return <div>Loading...</div>
+
+  return (
+    <div className="p-4">
+      <div className="grid gap-4">
+        <div className="mb-4 flex pb-4 border-b">
+          <div className='flex flex-grow flex-row'>
+            <Bookmark className="h-16 w-16 me-2 inlince-block" />
+            <h1 className="self-center text-3xl font-bold text-gray-900 dark:text-gray-100 inline-block">Bookmarks</h1>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 gap-4">
+          {wishlist.map((stock) => (
+            <Card key={stock} onClick={() => router.push(`/details/${stock}`)}>
+              <CardHeader>
+                <CardTitle>{names.find((n) => n.stock == stock)!.name} ({stock})</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid grid-cols-2 gap-4">
+                  <div>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">Predicted Direction</p>
+                    <p className={`text-lg font-semibold text-${forecast.find((f) => f.stock == stock)!.forecast == "UP" ? "green" : "red"}-500`}>{forecast.find((f) => f.stock == stock)!.forecast}</p>
+                  </div>
+                  <div>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">Predicted Change</p>
+                    <p className={`text-lg font-semibold text-${forecast.find((f) => f.stock == stock)!.forecast == "UP" ? "green" : "red"}-500`}>{forecast.find((f) => f.stock == stock)!.forecast == "UP" ? "" : "-"}{forecast.find((f) => f.stock == stock)!.percent}%</p>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
